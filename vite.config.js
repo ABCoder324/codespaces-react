c2916d64f1a2ac49b662886d357d424fe5096b15
ABCoder324 <codeid128@gmail.com>
2025-09-17 00:18:54 +0000

    Done

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/codespaces-react/", // Replace with your repository name
   test: {
     globals: true,
     environment: 'jsdom',
